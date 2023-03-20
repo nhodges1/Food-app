a62704a592c92074c19969b3baf5dc289075443a
Joe <joe7663@gmail.com>
2023-03-20 16:10:25 -0400

    Application and pom.xml

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,2 +1,15 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.logging.LogManager;
+
+@SpringBootApplication
 public class Application {
+    public static Logger log = LogManager.getLogger();
+
+    public static void main(String[] args){
+        SpringApplication.run(Application.class);
+    }
 }
