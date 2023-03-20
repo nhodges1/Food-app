ad0deee29c4c6b5e600212fbc591ad15704b2609
Manjukh <manjukasthuri@yahoo.com>
2023-03-20 16:33:50 -0400

    Application and pom.xml

--- src/main/java/Model/Fooditem.java
+++ src/main/java/Model/Fooditem.java
@@ -1,4 +1,26 @@
 package Model;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.util.List;
+
+@Entity
+@Data
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
 public class Fooditem {
+    private int foodId;
+    private String foodName;
+    private int categoryId;
+    private String categoryName;
+    private String description;
+    private int priceOfItem;
+    private String foodTag;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    @JsonManagedReference
+    private List<Fooditem> fooditem;
 }
