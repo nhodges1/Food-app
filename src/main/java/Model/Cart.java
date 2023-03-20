39e28646dc79fc2baa8d5152bc70da9f23902f85
Joe <joe7663@gmail.com>
2023-03-20 15:49:23 -0400

    Cart User models

--- src/main/java/Model/Cart.java
+++ src/main/java/Model/Cart.java
@@ -1,4 +1,49 @@
 package Model;
 
 public class Cart {
+    public int user_id;
+    public int cart_id;
+    public int item_id;
+    public int quantity;
+
+    public Cart(){}
+
+    public Cart(int user_id, int cart_id, int item_id, int quantity){
+        this.user_id = user_id;
+        this.cart_id = cart_id;
+        this.item_id = item_id;
+        this.quantity = quantity;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getCart_id() {
+        return cart_id;
+    }
+
+    public void setCart_id(int cart_id) {
+        this.cart_id = cart_id;
+    }
+
+    public int getItem_id() {
+        return item_id;
+    }
+
+    public void setItem_id(int item_id) {
+        this.item_id = item_id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
--- src/main/java/Model/User.java
+++ src/main/java/Model/User.java
@@ -1,4 +1,49 @@
 package Model;
 
 public class User {
+    public int userId;
+    public String userName;
+    public String userEmail;
+    public String password;
+
+    public User(){
+
+    }
+    public User(int userId, String userName, String userEmail, String password){
+        this.userId = userId;
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.password = password;
+    }
+    public int getUserId(){
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
