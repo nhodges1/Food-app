35f3bb88f870c10bbd9b1f1429d21f18d4db7d3f
Joe <joe7663@gmail.com>
2023-03-20 16:24:41 -0400

    App controller and workspace

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,8 +3,6 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.logging.LogManager;
-
 @SpringBootApplication
 public class Application {
     public static Logger log = LogManager.getLogger();
