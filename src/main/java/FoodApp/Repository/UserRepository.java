702d233938b197c9dc4e0e84d99c40a83b98c598
Joe <joe7663@gmail.com>
2023-03-21 13:33:25 -0400

    user optionals fixed

--- src/main/java/FoodApp/Repository/UserRepository.java
+++ src/main/java/FoodApp/Repository/UserRepository.java
@@ -5,12 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long>{
     @Query("FROM users WHERE name = :name")
-    User getUserByName(@Param("name") String name);
-
-    @Query("FROM users WHERE user = :user")
-    User addAccount(@Param("user") User user);
+    Optional<User> getUserByName(@Param("name") String name);
 }
 
 
